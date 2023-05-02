2b4f181eab9bd17146202d76c1953f3ff1c73348
angelsoempty <denatamanov1@gmail.com>
2023-05-02 17:12:59 +0300

    #4

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 def func(*args):
-    s = {arg: arg ** 2 for arg in args}
-    return s
-
-result = func(1, 2, 3, 4, 5)
+    odd_num = []
+    for j in args:
+        if j % 2 != 0:
+            odd_num.append(j)
+    return odd_num
+result = func(1,2,3,4,5,6)
 print(result)
